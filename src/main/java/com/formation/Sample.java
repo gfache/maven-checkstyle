4f4148701460473c3c1f0af0e0617ea2b1f6391f
gfache <g.fache@free.fr>
2019-04-29 16:02:02 +0200

    Update Sample.java

--- src/main/java/com/formation/Sample.java
+++ src/main/java/com/formation/Sample.java
@@ -1,6 +1,12 @@
 package com.formation;
 
-@author Guillaume Fache
+/**
+* Validator used to check whether given string is
+* no longer than the specified amount of characters.
+*
+* @author Guillaume Fache
+*/
+
 public class Sample {
 	
 	public Boolean shouldBeWarning() {
